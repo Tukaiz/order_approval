0f1fecc925d2add3039775e487cdc49e09aa1560
tatums <tatum@ashlandstudios.com>
2015-01-13 12:16:21 -0600

    make order readable

--- lib/order_approval.rb
+++ lib/order_approval.rb
@@ -21,11 +21,13 @@ module OrderApproval
 
   module Authorization
     module Permissions
-
+      ## This a global approver.  With this permission.
+      ## can view all orders that need approving
+      ## && can approve or reject the order.
       def can_manage_order_holds
         ## Can view the approvals section
         can :view_order_approval_section, Order
-
+        can :read, Order
         can :manage, OrderHold
       end
 
@@ -37,6 +39,10 @@ module OrderApproval
         ## approval from.
         UserEditContext.call(@user, @site)
         ids = @user.full_claims.map { |claim| claim.id }
+
+        ## Can read any order for which there is at least 1 order_hold that needs approval
+        can :read, Order, id: Order.for_approvals(ids).pluck(:id)
+
         can :view, OrderHold, claim_id: ids
         can :read, OrderHold, claim_id: ids
         can :manage, OrderHold, claim_id: ids
